d95ecea1eb379e6f5d04ec6e2e4969d14bcbf1bd
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 09:30:13 +0200

    prova porta

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        //porta in ascolto
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+        Socket miSocket = mioServerSocket.accept();
+        System.out.println("Qualcuno si è collegato");  
+        
+
+        
     }
 }
